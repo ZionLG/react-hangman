303c0df832ec6abc592582858005e349eb11c64c
LGGC <liran12369@gmail.com>
2022-11-26 12:17:26 +0200

    add skeleton files and basic game state

---
+++ READNE.md
@@ -0,0 +1,1 @@
+React Hangman is a simple React project built using Typescript and Tailwind CSS
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import words from "./wordList.json"
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default App
